da873fc2514cb7591ef2285bd5227047d4f1b31a
VSH Kiran <saiharikiran@outlook.com>
2024-06-05 20:57:15 +0530

    upgraded update feature witj useFormState

--- app/ui/update-ui.jsx
+++ app/ui/update-ui.jsx
@@ -2,15 +2,19 @@
 
 import Button from "./button";
 import { updateCMS } from "../lib/actions";
-import { useFormStatus } from "react-dom";
+import { useFormState } from "react-dom";
+
+const initialState = {
+  success: false,
+};
 
 export default function UpdateUi() {
-  const { pending } = useFormStatus();
-  console.log(pending);
+  const [state, formAction] = useFormState(updateCMS, initialState);
+  console.log(state);
   return (
     <div className="flex justify-between items-center custom-border bg-white p-3 mb-5">
       <p>New Update Available</p>
-      <form action={updateCMS}>
+      <form action={formAction}>
         <Button name="Update Now" />
       </form>
     </div>
