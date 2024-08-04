d51c36eb4fcfdd23bd285fe419840d6ef58a124b
VSH Kiran <saiharikiran@outlook.com>
2024-08-04 20:53:45 +0530

    shell script updated

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-git pull origin dev
+git pull origin dev 2>&1 || { echo "Git pull failed"; exit 1; }
 
-npm install
+npm install 2>&1 || { echo "npm install failed"; exit 1; }
 
-npm run build
+npm run build 2>&1 || { echo "npm run build failed"; exit 1; }
